f9afe24b599ba700cee62b951825439936ff9f08
Tobias Eriksson <toberi@outlook.com>
2021-11-08 21:36:41 +0100

    Fixed errors

--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -1,24 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models;
+using ProductApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<ProductRepository>();
 var app = builder.Build();
 
-app.MapPost("/products", ([FromServices] ProductRepository repository, Product product) =>
+app.MapPost("/products", (ProductRepository repository, Product product) =>
 {
     repository.CreateProduct(product);
     return Results.Created($"/products/{product.Id}", product);
 });
 
-app.MapGet("/products", async ([FromServices] ProductRepository repository) => { await Task.Delay(100); return repository.GetAllProducts(); });
+app.MapGet("/products", async (ProductRepository repository) => { await Task.Delay(100); return repository.GetAllProducts(); });
 
-app.MapGet("/products/{id}", ([FromServices] ProductRepository repository, Guid id) =>
+app.MapGet("/products/{id}", (ProductRepository repository, Guid id) =>
 {
     var product = repository.GetProductById(id);
     return product is not null ? Results.Ok(product) : Results.NotFound();
 });
 
-app.MapPut("/products/{id}", ([FromServices] ProductRepository repository, Guid id, Product updatedProduct) =>
+app.MapPut("/products/{id}", (ProductRepository repository, Guid id, Product updatedProduct) =>
 {
     var product = repository.GetProductById(id);
     if (product is null) { return Results.NotFound(); }
@@ -26,7 +27,7 @@ app.MapPut("/products/{id}", ([FromServices] ProductRepository repository, Guid 
     return Results.Ok(updatedProduct);
 });
 
-app.MapDelete("/products/{id}", ([FromServices] ProductRepository repository, Guid id) =>
+app.MapDelete("/products/{id}", (ProductRepository repository, Guid id) =>
 {
     repository.DeleteProduct(id);
     return Results.Ok();
