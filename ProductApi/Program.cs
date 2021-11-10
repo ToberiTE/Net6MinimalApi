5ebea360555c0f5bf679750c7086139321be99a0
Tobias Eriksson <toberi@outlook.com>
2021-11-10 18:35:19 +0100

    Added swagger to project

--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -2,34 +2,42 @@ using ProductApi.Models;
 using ProductApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 builder.Services.AddSingleton<ProductRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapPost("/products", (ProductRepository repository, Product product) =>
 {
-    repository.CreateProduct(product);
+    repository.Create(product);
     return Results.Created($"/products/{product.Id}", product);
 });
 
-app.MapGet("/products", async (ProductRepository repository) => { await Task.Delay(100); return repository.GetAllProducts(); });
+app.MapGet("/products", async (ProductRepository repository) => { await Task.Delay(100); return repository.GetAll(); });
 
 app.MapGet("/products/{id}", (ProductRepository repository, Guid id) =>
 {
-    var product = repository.GetProductById(id);
+    var product = repository.GetById(id);
     return product is not null ? Results.Ok(product) : Results.NotFound();
 });
 
 app.MapPut("/products/{id}", (ProductRepository repository, Guid id, Product updatedProduct) =>
 {
-    var product = repository.GetProductById(id);
+    var product = repository.GetById(id);
     if (product is null) { return Results.NotFound(); }
-    repository.UpdateProduct(updatedProduct);
+    repository.Update(updatedProduct);
     return Results.Ok(updatedProduct);
 });
 
 app.MapDelete("/products/{id}", (ProductRepository repository, Guid id) =>
 {
-    repository.DeleteProduct(id);
+    repository.Delete(id);
     return Results.Ok();
 });
 
